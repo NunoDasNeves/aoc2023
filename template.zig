c18ff61a221442509d437903c7901e23f66e0870
Nuno Das Neves <ndasneves@gmail.com>
2023-12-06 20:43:15 -0800

    update template

--- template.zig
+++ template.zig
@@ -6,6 +6,9 @@ const parseUnsigned = std.fmt.parseUnsigned;
 const trim = std.mem.trim;
 const ascii = std.ascii;
 
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+const m = gpa.allocator();
+
 fn strTokAny(str: []const u8, delimiters: []const u8) std.mem.TokenIterator(u8, .any) {
     const tokenizeAny = std.mem.tokenizeAny;
     return tokenizeAny(u8, str, delimiters);
@@ -30,19 +33,13 @@ fn strTrim(str: []const u8, values: []const u8) []const u8 {
     return trim(u8, str, values);
 }
 
-var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-const m = gpa.allocator();
-
 fn getInput() ![]const u8 {
     const args = try std.process.argsAlloc(m);
     defer std.process.argsFree(m, args);
-    const f = try std.fs.cwd().openFile(args[1], .{ .mode = .read_only });
-    defer f.close();
-    return try f.readToEndAlloc(m, std.math.maxInt(usize));
+    return std.fs.cwd().readFileAlloc(m, args[1], std.math.maxInt(usize));
 }
 
 pub fn main() !void {
     const input = try getInput();
-
     print("{s}\n", .{input});
 }
