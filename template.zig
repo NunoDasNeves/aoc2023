04644a9d19081186ed2d5ba375b01f3e3cd0fdfc
Nuno Das Neves <ndasneves@gmail.com>
2023-12-16 20:50:07 -0800

    update template with solve fn

--- template.zig
+++ template.zig
@@ -10,11 +10,22 @@ const ascii = std.ascii;
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 const m = gpa.allocator();
 
-pub fn main() !void {
-    const input = try util.getInput();
+fn solve(input: []const u8, part2: bool) !usize {
+    var total: usize = 0;
     var line_it = util.strTokLine(input);
+
     while (line_it.next()) |line| {
+        total += 1;
         _ = line;
     }
-    print("{s}\n", .{input});
+
+    if (part2) {}
+
+    return total;
+}
+
+pub fn main() !void {
+    const input = try util.getInput();
+    print("{s}\n", .{try solve(input, false)});
+    //print("{s}\n", .{try solve(input, true)});
 }
